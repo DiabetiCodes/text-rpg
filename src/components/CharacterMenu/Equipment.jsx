0da07b23e04a7e77813ae62e3388c52afc607856
Michael Mastrucci <michael.mastrucci@gmail.com>
2025-02-08 11:10:45 -0500

    component added with sensible person body layout

--- src/components/CharacterMenu/Equipment.jsx
+++ src/components/CharacterMenu/Equipment.jsx
@@ -5,19 +5,20 @@ const Equipment = ({ weaponSlot, shieldSlot, armorSlot, helmetSlot, ringSlot, ma
 		<div className='w-[400px] max-w-[400px] mx-auto'>
 			<h1>Equipment</h1>
 			<hr />
-			<div class='rpgui-icon weapon-slot'></div>
+			<div class='rpgui-icon'></div>
+			<div class='rpgui-icon helmet-slot'></div>
+			<div class='rpgui-icon'></div>
+			<br />
+			<br />
 			<div class='rpgui-icon shield-slot'></div>
 			<div class='rpgui-icon armor-slot'></div>
+			<div class='rpgui-icon weapon-slot'></div>
 			<br />
-			<br />
-
-			<div class='rpgui-icon helmet-slot'></div>
+      <br />
+			<div class='rpgui-icon ring-slot'></div>
+			<div class='rpgui-icon shoes-slot'></div>
 			<div class='rpgui-icon ring-slot'></div>
 			<br />
-			<br />
-
-			<div class='rpgui-icon shoes-slot'></div>
-			<div class='rpgui-icon empty-slot'></div>
 		</div>
 	);
 };
